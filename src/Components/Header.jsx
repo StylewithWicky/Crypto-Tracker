9504194b46e22bb6ebc9155f4ee8d71a337b8bf5
Yazz <itsjustmaitai@gmail.com>
2025-05-03 17:55:52 +0300

    Styled the header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Logo from './Logo'
 import Searchbar from './Searchbar'
-
+import '../Styles/Header.css'
 function Header() {
   return (
     <div className='Header'>
